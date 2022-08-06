119f5ec5a5f4925efa93458f32a9e1b330758f24
Adam <fallenempire.tv@gmail.com>
2022-08-05 17:04:29 -0700

    modification to navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 
+import { NavLink } from "react-router-dom";
+
 import styles from "./styles.module.css";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar_container}>
       <div className={styles.navbar}>
-        <ul className={styles.nav_items}>
-          <li className={styles.nav_item}>Home</li>
-          <li className={styles.nav_item}>Services</li>
-          <li className={styles.nav_item}>Contact</li>
-          <li className={styles.nav_item}>Our Process</li>
-          <li className={styles.nav_item}>Portfolio</li>
-        </ul>
+        <div className={styles.nav_items}>
+          <NavLink className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)} to="/">
+            Home
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)} to="/industries">
+            Services
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)} to="/contact">
+            Contact
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)}
+            to="/our-process"
+          >
+            Our Process
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)} to="/portfolio">
+            Portfolio
+          </NavLink>
+        </div>
       </div>
     </div>
   );
