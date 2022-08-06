2d49bff1df6f7637419025817662cc55897488db
Adam <fallenempire.tv@gmail.com>
2022-08-05 17:08:26 -0700

    services page and addition of showcase component

---
+++ src/components/Showcase/index.jsx
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+
+import { motion, useInView } from "framer-motion";
+
+import styles from "./styles.module.css";
+
+const Showcase = ({ name, img, reasons }) => {
+  const ref = useRef(null);
+
+  const inView = useInView(ref);
+
+  return (
+    <section
+      ref={ref}
+      style={{
+        width: inView ? "100%" : "50%",
+        transform: inView ? "none" : "translateX(200px)",
+        opacity: inView ? 1 : 0,
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
+      }}
+    >
+      <div className={styles.card_container}>
+        <div className={styles.card_wrapper}>
+          <div className={styles.background_img} style={{ backgroundImage: `url(${img})` }}></div>
+          <div className={styles.left_col}>
+            <h1>{name}</h1>
+            <motion.a whileHover={{ scale: 1.1 }} className={styles.discover_btn}>
+              Discover More
+            </motion.a>
+          </div>
+          <div className={styles.right_col}>
+            <div className={styles.reason_container}>
+              <ul>
+                {reasons.length > 0 &&
+                  reasons.map((el, i) => (
+                    <motion.li whileHover={{ x: 15 }} className={styles.reason} key={i}>
+                      {el}
+                    </motion.li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Showcase;
--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -1,17 +1,39 @@
-import React from "react";
-import Selections from "./Selections";
+import React, { useEffect, useState } from "react";
+import Showcase from "../../components/Showcase/index.jsx";
+
+// sanity client
+import { client, urlFor } from "../../lib/client.js";
 
 import styles from "./styles.module.css";
 
 const Services = () => {
+  const [serviceArr, setServiceArr] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const getData = async () => {
+      const q = '*[_type == "service"]';
+      const serviceData = await client.fetch(q);
+      if (mounted) {
+        setServiceArr(serviceData);
+      }
+    };
+    getData();
+
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div>
       <div className={styles.service_header}>
         <h1>Industries</h1>
         <h2>Served</h2>
       </div>
-      <Selections />
-      <>Selected</>
+      <div className={styles.card_container}>
+        {serviceArr.length > 0 &&
+          serviceArr.map((el, i) => <Showcase key={i} name={el?.name} reasons={el?.reasons} img={urlFor(el?.image)} />)}
+      </div>
     </div>
   );
 };
