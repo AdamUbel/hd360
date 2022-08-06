4f40b362cec82e2f905bfde9f3cff1da6b9045c6
Adam <fallenempire.tv@gmail.com>
2022-08-05 17:04:11 -0700

    modification to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
     <section id="footer">
       <div className={styles.footer_container}>
         <div className={styles.directory}>
-          <a>Home</a>
-          <a>Services</a>
-          <a>Contact</a>
-          <a>Our Process</a>
-          <a>Portfolio</a>
+          <a href="/">Home</a>
+          <a href="/industries">Services</a>
+          <a href="/contact">Contact</a>
+          <a href="/our-process">Our Process</a>
+          <a href="/portfoio">Portfolio</a>
         </div>
 
         <small className={styles.footer_cpr}>© 2021-2022 HD360 Productions. All rights reserved.</small>
