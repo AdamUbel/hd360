8b2c135327912317c7096d46b33750d27807d5c0
Adam <fallenempire.tv@gmail.com>
2022-08-07 13:59:04 -0700

    showcase width change why video css modification

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -13,8 +13,8 @@ const Showcase = ({ name, img, reasons }) => {
     <section
       ref={ref}
       style={{
-        width: inView ? "100%" : "50%",
-        transform: inView ? "none" : "translateX(200px)",
+        width: inView ? "100%" : "80%",
+        transform: inView ? "none" : "translateX(-200px)",
         opacity: inView ? 1 : 0,
         transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
       }}
