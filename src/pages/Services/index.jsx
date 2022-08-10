6ad13426cdf62e1ace7a18a621903d37eac2d537
Adam <fallenempire.tv@gmail.com>
2022-08-09 20:05:15 -0700

    framer motion and CSS changes

--- src/pages/Services/index.jsx
+++ src/pages/Services/index.jsx
@@ -4,6 +4,8 @@ import Showcase from "../../components/Showcase/index.jsx";
 // sanity client
 import { client, urlFor } from "../../lib/client.js";
 
+import { motion } from "framer-motion";
+
 import styles from "./styles.module.css";
 
 const Services = () => {
@@ -30,9 +32,22 @@ const Services = () => {
         <h1>Industries</h1>
         <h2>Served</h2>
       </div>
+      <div className={styles.service_explore}>
+        <h1>
+          Explore Our <span>Areas Of Expertise</span> Below.
+        </h1>
+      </div>
       <div className={styles.card_container}>
         {serviceArr.length > 0 &&
           serviceArr.map((el, i) => <Showcase key={i} name={el?.name} reasons={el?.reasons} img={urlFor(el?.image)} />)}
+      </div>
+      <div className={styles.service_explore}>
+        <h2>Any Addtional Questions?</h2>
+        <h3>
+          Reach Out To Us <span>AnyTime</span>.
+        </h3>
+        <motion.p whileHover={{ scale: 1.2 }}>Email: info@hd360prod.com</motion.p>
+        <motion.p whileHover={{ scale: 1.2 }}>Phone: (908) 801-6265 </motion.p>
       </div>
     </div>
   );
