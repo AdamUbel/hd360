6e710d6b96c055579a05976cf6e16846422c42e1
Adam <fallenempire.tv@gmail.com>
2022-08-09 20:05:35 -0700

    section removal and CSS changes

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -10,15 +10,7 @@ const Showcase = ({ name, img, reasons }) => {
   const inView = useInView(ref);
 
   return (
-    <section
-      ref={ref}
-      style={{
-        width: inView ? "100%" : "80%",
-        transform: inView ? "none" : "translateX(-200px)",
-        opacity: inView ? 1 : 0,
-        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
-      }}
-    >
+    <section>
       <div className={styles.card_container}>
         <div className={styles.card_wrapper}>
           <div className={styles.background_img} style={{ backgroundImage: `url(${img})` }}></div>
@@ -30,7 +22,15 @@ const Showcase = ({ name, img, reasons }) => {
           </div>
           <div className={styles.right_col}>
             <div className={styles.reason_container}>
-              <ul>
+              <ul
+                ref={ref}
+                style={{
+                  width: inView ? "100%" : "80%",
+                  transform: inView ? "none" : "translateX(-200px)",
+                  opacity: inView ? 1 : 0,
+                  transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
+                }}
+              >
                 {reasons.length > 0 &&
                   reasons.map((el, i) => (
                     <motion.li whileHover={{ x: 15 }} className={styles.reason} key={i}>
