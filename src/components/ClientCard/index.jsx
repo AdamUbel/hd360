73e11310ac6d84f92d569f8d33436c728d2552b6
Adam <fallenempire.tv@gmail.com>
2022-08-12 17:40:51 -0700

    implimentation of client img's and CSS changes

--- src/components/ClientCard/index.jsx
+++ src/components/ClientCard/index.jsx
@@ -3,15 +3,26 @@ import React from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 import { HiStar, HiOutlineStar } from "react-icons/hi";
 
+import { urlFor } from "../../lib/client";
+
 import styles from "./styles.module.css";
 
-const ClientCard = ({ name, rating, review }) => {
+const ClientCard = ({ image, name, rating, review }) => {
   const ratingArr = [1, 2, 3, 4, 5];
+  let imgurl = "";
+
+  if (image) {
+    imgurl = urlFor(image);
+  }
 
   return (
     <div className={styles.card}>
       <div className={styles.card_client}>
-        <FaRegUserCircle className={styles.card_client_img} />
+        {imgurl !== "" ? (
+          <img src={imgurl} alt="Client Picture" className={styles.client_img} />
+        ) : (
+          <FaRegUserCircle className={styles.card_client_img} />
+        )}
         <h2>{name}</h2>
         <div>
           {ratingArr.map((el, i) => {
