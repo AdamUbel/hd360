c76e0e26553cd7ff7c8a7f5676859c65be1e7b6a
Adam <fallenempire.tv@gmail.com>
2022-08-16 17:51:04 -0700

    addition to dropdown - not 100%

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavLink } from "react-router-dom";
+
+import { MdKeyboardArrowRight, MdKeyboardArrowDown } from "react-icons/md";
 
 import styles from "./styles.module.css";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className={styles.navbar_container}>
       <div className={styles.navbar}>
@@ -12,9 +16,27 @@ const Navbar = () => {
           <NavLink className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)} to="/">
             Home
           </NavLink>
-          <NavLink className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)} to="/industries">
+          <NavLink
+            onMouseLeave={() => setShow(false)}
+            onMouseOver={() => setShow(true)}
+            className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)}
+            to="/industries"
+          >
             Services
+            {show ? (
+              <MdKeyboardArrowDown style={{ position: "relative", transform: "translateY(25%)" }} />
+            ) : (
+              <MdKeyboardArrowRight style={{ position: "relative", transform: "translateY(25%)" }} />
+            )}
+            {show && (
+              <div className={styles.drop_down}>
+                <NavLink to={"/business"}>Business</NavLink>
+                <NavLink to={"/equine"}>Equine</NavLink>
+                <NavLink to={"/events"}>Events</NavLink>
+              </div>
+            )}
           </NavLink>
+
           <NavLink className={({ isActive }) => (isActive ? styles.nav_item_active : styles.nav_item)} to="/contact">
             Contact
           </NavLink>
