d25fe6789352f90d2df79a4c19b7940ce35bff5f
Adam <fallenempire.tv@gmail.com>
2022-08-16 17:09:56 -0700

    Changes to service cards CSS - still not 100%

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -1,10 +1,13 @@
 import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 
 import { motion, useInView } from "framer-motion";
 
+import { AiOutlineDoubleRight } from "react-icons/ai";
+
 import styles from "./styles.module.css";
 
-const Showcase = ({ name, img, reasons }) => {
+const Showcase = ({ name, img, reasons, route }) => {
   const ref = useRef(null);
 
   const inView = useInView(ref);
@@ -17,29 +20,33 @@ const Showcase = ({ name, img, reasons }) => {
           <div className={styles.left_col}>
             <div className={styles.left_wrapper}>
               <h1>{name}</h1>
-              <motion.a whileHover={{ scale: 1.1 }} className={styles.discover_btn}>
-                Discover More
-              </motion.a>
+              <div className={styles.reason_container}>
+                <ul
+                  ref={ref}
+                  style={{
+                    width: inView ? "100%" : "80%",
+                    transform: inView ? "none" : "translateX(-200px)",
+                    opacity: inView ? 1 : 0,
+                    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
+                  }}
+                >
+                  {reasons.length > 0 &&
+                    reasons.map((el, i) => (
+                      <motion.li whileHover={{ x: 15 }} className={styles.reason} key={i}>
+                        {el}
+                      </motion.li>
+                    ))}
+                </ul>
+              </div>
             </div>
           </div>
           <div className={styles.right_col}>
-            <div className={styles.reason_container}>
-              <ul
-                ref={ref}
-                style={{
-                  width: inView ? "100%" : "80%",
-                  transform: inView ? "none" : "translateX(-200px)",
-                  opacity: inView ? 1 : 0,
-                  transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s",
-                }}
-              >
-                {reasons.length > 0 &&
-                  reasons.map((el, i) => (
-                    <motion.li whileHover={{ x: 15 }} className={styles.reason} key={i}>
-                      {el}
-                    </motion.li>
-                  ))}
-              </ul>
+            <div>
+              <Link to={`/${route}`} className={styles.discover_btn}>
+                <motion.h3 whileHover={{ scale: 1.05 }}>
+                  Discover More <AiOutlineDoubleRight className={styles.discover_arrow} />
+                </motion.h3>
+              </Link>
             </div>
           </div>
         </div>
