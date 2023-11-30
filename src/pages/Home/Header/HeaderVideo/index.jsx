78d2e2797e33207c9bc425c3b2fba4458438f945
Adam <92753806+AdamUbel@users.noreply.github.com>
2023-11-29 17:48:20 -0800

    commented out sanity video api due to bandwith

--- src/pages/Home/Header/HeaderVideo/index.jsx
+++ src/pages/Home/Header/HeaderVideo/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import styles from "./styles.module.css";
 
+import VIDEO from "../../../../assets/videos/file.mp4";
+
 const HeaderVideo = ({ url }) => {
   return (
     <video autoPlay playsInline loop muted id={styles.video}>
-      <source src={url} type="video/mp4" />
+      <source src={VIDEO} type="video/mp4" />
+      {/* NOTE sanity video from API call */}
+      {/* <source src={url} type="video/mp4" /> */}
     </video>
   );
 };
